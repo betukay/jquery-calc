ef40715b71f61272d8406ff6be1825a2df74d20b
Betu <Home@Betus-MacBook-Air.local>
2018-02-23 14:01:44 -0500

    add javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 var display = document.getElementById('display');
+if(display.value.length = Number(16)){
+  display.value = display.value.substring(0,16);
+}
 
-display = 5;
+
+function toDisplay(x){
+  display.value += x;
+  if(x==="AC"){
+    display.value = "";
+    }
+  if(display.value.charAt(0) === "0" && display.value.length === Number("2")){
+    display.value = display.value.substr(1);
+  }
+}
+
+function remove(){
+  var num = display.value;
+  var len = num.length - 1;
+  var newNum = num.substring(0,len);
+  display.value = newNum;
+}
+
+
+
+function answer(){
+  if(display.value===""){
+    display.value = "0";
+  }
+  else{
+    x=display.value;
+    x=eval(x);
+    display.value=x;
+  }
+}
+
+function power(){
+  x=display.value;
+  x=eval(x*x);
+  display.value=x;
+}
